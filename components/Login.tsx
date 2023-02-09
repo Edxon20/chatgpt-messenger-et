e4bb6487fe5b902ed2a233a3135b54380b3372d1
John Doe <edixon20pes@gmail.com>
2023-02-09 11:24:15 -0400

    [ADD] Credentiales Google Cloud

--- components/Login.tsx
+++ components/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
         height={300}
         alt="logo"
       />
-      <button className="text-white font-bold 
+      <button onClick={() => signIn("google")}  className="text-white font-bold 
       text-3xl animate-pulse">
         Sign In to use ChatGPT 
       </button>
