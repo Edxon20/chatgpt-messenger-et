df01f9f21ee794ae324022b54cec69ca5cd8784b
John Doe <edixon20pes@gmail.com>
2023-02-10 09:56:42 -0400

    [ADD] Chat.tsx and Chatinput.tsx

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,19 @@
+import Chat from "../../../components/Chat";
+import ChatInput from "../../../components/Chat";
 
-function ChatPage() {
+type Props ={
+  params: {
+    id: string,
+  }
+}
+
+function ChatPage({params: {id}} : Props) {
+  
   return (
-    <div>ChatPage</div>
+    <div className="flex flex-col h-screen overflow-hidden">
+        < Chat chatId={id}/>
+        < ChatInput chatId={id}/>
+    </div>
   )
 }
 
